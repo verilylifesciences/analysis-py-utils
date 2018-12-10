9496f281ee46489712b85837b60adf9cd4eb9708
bcaine <bcaine@google.com>
2018-12-10 09:20:10 -0500

    Update google-cloud-bigquery version to incorporate PollingFuture retry changes.
    Specifically: https://github.com/googleapis/google-cloud-python/commit/f576d148f802666dbd11cf7c50a0629a77f81665
    
    Change-Id: Id3244786ffa73cbe3a166cfe35cb7f2219140a31

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 REQUIRED_PACKAGES = ['pandas',
                      'google-api-core==1.5.1',
                      'google-auth==1.4.1',
-                     'google-cloud-bigquery==1.6.0',
+                     'google-cloud-bigquery==1.7.0',
                      'google-cloud-storage==1.10.0',
                      'pysqlite>=2.8.3',
                      'ddt',
